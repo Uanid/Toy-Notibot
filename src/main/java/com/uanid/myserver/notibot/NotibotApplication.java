ff6dc9645bad7a2b8e36169ac21f99c74db9ea07
Minuk Song <uanid@outlook.com>
2019-11-10 15:55:19 +0900

    첫 번째 커밋

---
+++ src/main/java/com/uanid/myserver/notibot/NotibotApplication.java
@@ -0,0 +1,13 @@
+package com.uanid.myserver.notibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotibotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotibotApplication.class, args);
+    }
+
+}
